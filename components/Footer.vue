7e6ea5868e84b99fcdeabd46802b5d56b80484b4
axelle <kadance9720@gmail.com>
2021-10-24 17:52:11 +0200

    Add Footer

---
+++ components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <p class="text-center">© Copyright.<nuxt-link class="text-primary" to="/nous" target="_blank">Nous</nuxt-link></p>
+            </div>
+        </div>
+    </div>
+</template>
--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,15 +1,12 @@
 <template>
     <div>
         <nav class="navbar navbar-expand-md navbar-dark bg-info">
-            <ul class="navbar-nav">
+            <ul class="navbar-nav mx-auto">
                 <li class="nav-item">
                     <nuxt-link class="nav-link" aria-current="page" to="/">Accueil</nuxt-link>
                 </li>
                 <li class="nav-item">
                     <nuxt-link class="nav-link" aria-current="page" to="/commandes">Commande</nuxt-link>
-                </li>
-                <li class="nav-item">
-                    <nuxt-link class="nav-link" aria-current="page" to="/nous">Nous</nuxt-link>
                 </li>
             </ul>
         </nav>
