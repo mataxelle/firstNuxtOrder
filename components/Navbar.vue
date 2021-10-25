14dbc3db07f23c0f947777d6c9130bdc36b0a219
axelle <kadance9720@gmail.com>
2021-10-25 14:20:24 +0200

    Update Home page and navbar, add image

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-        <nav class="navbar navbar-expand-md navbar-dark bg-info">
-            <ul class="navbar-nav mx-auto">
+        <nav class="navbar navbar-expand navbar-dark bg-info">
+            <ul class="navbar-nav mx-auto flex-row">
                 <li class="nav-item">
                     <nuxt-link class="nav-link" aria-current="page" to="/">Accueil</nuxt-link>
                 </li>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,116 @@
 <template>
-  <h1>Accueil</h1>
+  <div>
+    <div class="jumbotron my-5 change_background">
+      <h1 class="display-4">Bienvenue et bonjour, à tous!</h1>
+      <p class="lead">Profitez dès à présent de tout l'univers NuxtOrder !!</p>
+      <hr class="my-4" />
+      <p>Avec NuxtOrder, je consomme local.</p>
+      <nuxt-link class="btn btn-outline-light btn-lg" to="/commandes" role="button"
+        >Commander</nuxt-link
+      >
+    </div>
+
+    <h2>Nos services</h2>
+
+    <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4">
+      <div class="col mb-4">
+        <div class="card h-100">
+          <img
+            src="@/assets/images/service-image1.jpg"
+            class="card-img-top"
+            alt="Service image"
+          />
+          <div class="card-body">
+            <h5 class="card-title">Vendeurs partenaires</h5>
+            <p class="card-text">
+              This is a longer card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </p>
+          </div>
+          <div class="card-footer">
+            <p class="text-right">
+              <a href="#" class="text-primary">En savoir plus ></a>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col mb-4">
+        <div class="card h-100">
+          <img
+            src="@/assets/images/service-image2.png"
+            class="card-img-top"
+            alt="Service image"
+          />
+          <div class="card-body">
+            <h5 class="card-title">Satisfait ou remboursé</h5>
+            <p class="card-text">This is a short card.</p>
+          </div>
+          <div class="card-footer">
+            <p class="text-right">
+              <a href="#" class="text-primary">En savoir plus ></a>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col mb-4">
+        <div class="card h-100">
+          <img
+            src="@/assets/images/service-image1.jpg"
+            class="card-img-top"
+            alt="Service image"
+          />
+          <div class="card-body">
+            <h5 class="card-title">Livraison express</h5>
+            <p class="card-text">
+              This is a longer card with supporting text below as a natural
+              lead-in to additional content.
+            </p>
+          </div>
+          <div class="card-footer">
+            <p class="text-right">
+              <a href="#" class="text-primary">En savoir plus ></a>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col mb-4">
+        <div class="card h-100">
+          <img
+            src="@/assets/images/service-image2.png"
+            class="card-img-top"
+            alt="Service image"
+          />
+          <div class="card-body">
+            <h5 class="card-title">Tous nos services</h5>
+            <p class="card-text">
+              This is a longer card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </p>
+          </div>
+          <div class="card-footer">
+            <p class="text-right">
+              <a href="#" class="text-primary">En savoir plus ></a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
+
+<style scoped>
+.change_background {
+  background-image: url("@/assets/images/vegetables.jpg");
+  background-position: center center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  color: white;
+}
+</style>
