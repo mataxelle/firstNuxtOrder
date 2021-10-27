f4fb573fccca301e64bb14e8ebbac531ebf85787
axelle <kadance9720@gmail.com>
2021-10-27 18:51:08 +0200

    Update Layouts default

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,3 +7,15 @@
     <Footer />
   </div>
 </template>
+
+<script>
+import Navbar from '@/components/Navbar.vue'
+import Footer from '@/components/Footer.vue'
+
+export default {
+  components: {
+    Navbar,
+    Footer
+  }
+}
+</script>
